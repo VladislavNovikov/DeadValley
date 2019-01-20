856e67afc64d9ae1bd226efb8a60ead5b7826298
VladislavNovikov <45494635+VladislavNovikov@users.noreply.github.com>
2019-01-20 16:55:01 +0300

    Add files via upload

---
+++ src/main/java/spring/controller/UserController.java
@@ -0,0 +1,15 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/User")
+public class UserController {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "ShowUser";
+    }
+}
