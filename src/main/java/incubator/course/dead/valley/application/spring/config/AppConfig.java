663d3597a0c046bda831b7c6e7f5451df8f7ab1a
akatomakhina <katomakhinaanastasiya@gmail.com>
2019-01-23 23:10:24 +0200

    Отладка запуска теста

--- src/main/java/incubator/course/dead/valley/application/spring/config/AppConfig.java
+++ src/main/java/incubator/course/dead/valley/application/spring/config/AppConfig.java
@@ -3,60 +3,30 @@ package incubator.course.dead.valley.application.spring.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@ComponentScan("incubator.course.dead.valley.application")
 @PropertySource("classpath:database.properties")
 public class AppConfig {
-    private String url;
-    private String user;
-    private String driver;
-    private String password;
+    private Environment env;
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getDriver() {
-        return driver;
-    }
-
-    public void setDriver(String driver) {
-        this.driver = driver;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
     }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setUrl(this.url);
-        driverManagerDataSource.setUsername(this.user);
-        driverManagerDataSource.setPassword(this.password);
-        driverManagerDataSource.setDriverClassName(this.driver);
+        driverManagerDataSource.setUrl(env.getProperty("spring.url"));
+        driverManagerDataSource.setUsername(env.getProperty("spring.user"));
+        driverManagerDataSource.setPassword(env.getProperty("spring.password"));
+        driverManagerDataSource.setDriverClassName(env.getProperty("spring.driver"));
         return driverManagerDataSource;
     }
 }
--- src/test/incubator/course/dead/valley/application/spring/dao/UserDAOImplTest.java
+++ src/test/incubator/course/dead/valley/application/spring/dao/UserDAOImplTest.java
@@ -9,21 +9,20 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import org.junit.Assert;
 import java.util.List;
 
-@RunWith(SpringRunner.class)
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration(classes = AppConfig.class)
 public class UserDAOImplTest {
 
     @Autowired
     private AppConfig config;
-
-    @Before
-    public void init(){
-
-    }
 
     @Test
     public void getAllUsers() {
