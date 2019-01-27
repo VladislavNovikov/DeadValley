8eb30bd6ca8f241bfc957afb25ca49dd0e0bb082
Владислав <https://github.com/VladislavNovikov/DeadValley>
2019-01-27 16:52:14 +0300

    Доделал стартовую страницу

--- src/main/java/incubator/course/dead/valley/application/spring/controller/UserController.java
+++ src/main/java/incubator/course/dead/valley/application/spring/controller/UserController.java
@@ -4,11 +4,14 @@ import incubator.course.dead.valley.application.spring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collections;
+
 @Controller
-@RequestMapping("/User")
+@RequestMapping("/ShowStartPage")
 public class UserController {
     private UserService service;
 
@@ -17,8 +20,22 @@ public class UserController {
         this.service = service;
     }
 
-    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
-        model.addAttribute("message", service.getAllUser());
+    @GetMapping
+    @RequestMapping("")
+    public String showStartPage(ModelMap model) {
         return "ShowUser";
     }
+    @RequestMapping(value = "/TheMostRichest",method = RequestMethod.GET)
+    public String theMostRichest(ModelMap model) {
+        model.addAttribute("theMostRichest", Collections.max(service.getAllUser(),
+                (o1,o2) -> o1.getAccount() - o2.getAccount()));
+        return "ShowUser";
+    }
+
+    @RequestMapping(value = "/AccountsSum",method = RequestMethod.GET)
+    public String accountsSum(ModelMap model) {
+        model.addAttribute("accountsSum", service.getAllUser().size());
+        return "ShowUser";
+    }
+
 }
--- src/test/incubator/course/dead/valley/application/spring/dao/UserDAOImplTest.java
+++ src/test/incubator/course/dead/valley/application/spring/dao/UserDAOImplTest.java
@@ -3,18 +3,13 @@ package incubator.course.dead.valley.application.spring.dao;
 
 import incubator.course.dead.valley.application.model.User;
 import incubator.course.dead.valley.application.spring.config.AppConfig;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import org.junit.Assert;
 import java.util.List;
 
 @RunWith( SpringJUnit4ClassRunner.class )
